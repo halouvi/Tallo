f356a7b2d1f8e7e33c9897bba73ed96b5c603fab
Arie Moldawsky <ariemol555@gmail.com>
2021-01-03 20:12:46 +0200

    modal improvement

--- src/components/Board/Card/Card.jsx
+++ src/components/Board/Card/Card.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react';
+
 import { useDrag, useDrop } from 'react-dnd';
-import {CardModal} from '../CardModal/CardModal'
+import { Link, withRouter } from 'react-router-dom';
 
-export const Card = ({ card, thisListIdx, thisCardIdx, handleDrop }) => {
-  const [isModal, setIsModal] = useState(false);
-
-  const onToggleModal = () => {
-    setIsModal(!isModal)
+const _Card = ({history, card, thisListIdx, thisCardIdx, handleDrop }) => {
+  
+  
+  const onOpenModal = () => {
+    history.push(`/board/modal/${card._id}`);
   }
 
   const [{ isOver }, drop] = useDrop({
@@ -31,13 +31,16 @@ export const Card = ({ card, thisListIdx, thisCardIdx, handleDrop }) => {
   return (
     <>
       <div ref={drop} className={isOver ? 'insert-here' : ''}></div>
-      {isModal? <CardModal card={card} closeModal={onToggleModal}></CardModal> : ''}
       {/* <div className={`card${isDragging ? ' is-dragging ' : ' '} ${isOver ? ' is-over' : ''}`} ref={drag}> */}
       <div className={`card${isDragging ? ' is-dragging ' : ''}`} ref={drag}>
-        <div onClick={onToggleModal} className="container" id={card._id} ref={drop}>
-          <span>{card._id}</span>
-        </div>
+        {/* <Link to={`/board/modal/${card._id}`}> */}
+          <div onClick={onOpenModal} className="container" id={card._id} ref={drop}>
+            <span>{card._id}</span>
+          </div>
+        {/* </Link> */}
       </div>
     </>
   )
 }
+
+export const Card = withRouter(_Card);
--- src/components/Board/CardModal/CardModal.jsx
+++ src/components/Board/CardModal/CardModal.jsx
@@ -1,16 +1,55 @@
-// import { useState } from 'react';
-// import { useDrag, useDrop } from 'react-dnd'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 
-export const CardModal = ({ closeModal, card }) => {
+class _CardModal extends Component {
+  state = {
+    currList: {},
+    currCard: {}
+  }
 
-  
+  componentDidMount() {
+    console.log(this.props.board);
+    this.loadCard();
+  }
 
-  return (
-    <div onClick={closeModal} className="modal-container">
-      <div className="modal-section">
-        <button onClick={closeModal}>X</button>
-      Card Modal! {card._id}
+
+  loadCard = () => {
+    const cardId = this.props.match.params.id;
+    console.log(cardId);
+    const lists = this.props.board.lists;
+    const list = lists.find((list, index) => {
+      return list.cards.find(card => card._id === cardId)
+    })
+    this.setState({ currList: list })
+    const currCard = list.cards.find(card => card._id === cardId)
+    this.setState({ currCard }, console.log(currCard))
+  }
+
+  render() {
+    return (
+      <div className="modal-section" >
+        <div className="modal-container">
+          <NavLink className="exit-btn" to="/board">X</NavLink>
+          <div className="modal-title">
+            <h3>{this.state.currCard.title}</h3>
+            <p className="modal-subtitle">in list <span>{this.state.currList.title}</span></p>
+          </div>
+          <div className="desc-container">
+            <h3>Description</h3>
+            <textarea placeholder="Add a more detailed description..." name="" id="" cols="55" rows="10"></textarea>
+            <button>Save</button> <button>X</button>
+          </div>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
+
+function mapStateToProps(state) {
+  return {
+    board: state.boardReducer.board
+  }
+}
+
+export const CardModal = connect(mapStateToProps)(_CardModal)
