ccfec1dc54a98be0318d973d400faf4eef939e74
Arie Moldawsky <ariemol555@gmail.com>
2021-01-06 14:53:33 +0200

    login signup working

--- src/service/UserService.js
+++ src/service/UserService.js
@@ -1,4 +1,4 @@
-import HttpService from './HttpService.js'
+import {httpService} from './HttpService.js'
 
 export default {
     login,
@@ -12,44 +12,44 @@ export default {
     resetUnreadBookings
 }
 function getUsers() {
-    return HttpService.get('user')
+    return httpService.get('user')
 }
 function getById(userId) {
-    return HttpService.get(`user/${userId}`)
+    return httpService.get(`user/${userId}`)
 }
 function remove(userId) {
-    return HttpService.delete(`user/${userId}`)
+    return httpService.delete(`user/${userId}`)
 }
 
 function update(user) {
-    return HttpService.put(`user/${user._id}`, user)
+    return httpService.put(`user/${user._id}`, user)
 }
 
 async function login(userCred) {
-    const user = await HttpService.post('auth/login', userCred)
+    const user = await httpService.post('auth/login', userCred)
     return _handleLogin(user)
 }
 async function signup(userCred) {
-    const user = await HttpService.post('auth/signup', userCred)
+    const user = await httpService.post('auth/signup', userCred)
     return _handleLogin(user)
 }
 async function logout() {
-    await HttpService.post('auth/logout');
+    await httpService.post('auth/logout');
     sessionStorage.clear();
 }
 
 async function unreadBooking(user) {
-    const updatedUser = await HttpService.put(`user/${user._id}`, user)
+    const updatedUser = await httpService.put(`user/${user._id}`, user)
     return _handleLogin(updatedUser);
 }
 
 async function resetUnreadBookings(user) {
-    const updatedUser = await HttpService.put(`user/reset/${user._id}`, user)
+    const updatedUser = await httpService.put(`user/reset/${user._id}`, user)
     return _handleLogin(updatedUser);
 }
 
 
 function _handleLogin(user) {
-    sessionStorage.setItem('user', JSON.stringify(user))
+    sessionStorage.setItem('loggedUser', JSON.stringify(user))
     return user;
 }
--- src/store/user/UserActions.js
+++ src/store/user/UserActions.js
@@ -1,10 +1,23 @@
-import { userService } from '../../service/UserService'
+import UserService from '../../service/UserService.js'
 
 export const types = {
   GET_USER_BY_ID: 'GET_USER_BY_ID',
+  SET_LOGGED_USER: 'SET_LOGGED_USER'
 }
 
-export const getUserById = id => async dispatch => {
-  const user = await userService.getById(id)
-  dispatch({ type: types.GET_USER_BY_ID, payload: user })
+export const GET_USER_BY_ID = id => async dispatch => {
+  const user = await UserService.getById(id);
+  return user;
+  // dispatch({ type: types.GET_USER_BY_ID, payload: user })
 }
+
+export const LOGIN = ({email, password}) => async dispatch => {
+  const user = await UserService.login({email, password});
+  dispatch({ type: types.SET_LOGGED_USER, user })
+}
+
+export const SIGNUP = ({name, email, password, imgUrl}) => async dispatch => {
+  const user = await UserService.signup({name, email, password, imgUrl});
+  dispatch({ type: types.SET_LOGGED_USER, user })
+}
+
