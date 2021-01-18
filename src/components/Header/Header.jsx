2623dd384d239928a6550c9671dd782e68b17bee
Arie Moldawsky <ariemol555@gmail.com>
2021-01-18 18:18:46 +0200

    little fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -18,7 +18,7 @@ export const Header = () => {
           <NavLink className="nav-item" to="/stats">Stats</NavLink>
         </nav>
         <div className="user-container">
-          {user?.imgUrl ? <img src={user.imgUrl} alt="" /> : <svg class="MuiSvgIcon-root MuiAvatar-fallback" focusable="false" viewBox="0 0 24 24" aria-hidden="true">
+          {user?.imgUrl ? <img src={user.imgUrl} alt="" /> : <svg focusable="false" viewBox="0 0 24 24" aria-hidden="true">
             <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z">
             </path>
           </svg>}
