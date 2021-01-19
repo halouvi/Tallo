93034784dd7fcb7e82b5de5675efc62b7e393bdf
Tal Harouvi <halouvi@gmail.com>
2021-01-19 15:32:05 +0200

    טקד

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -15,10 +15,9 @@ export const Header = () => {
           <NavLink className="add-board-btn nav-item" to="/create-modal" exact>+</NavLink>
           <NavLink className="nav-item" to="/" exact>Home</NavLink>
           <NavLink className="nav-item" to="/board">Board</NavLink>
-          <NavLink className="nav-item" to="/stats">Stats</NavLink>
         </nav>
         <div className="user-container">
-          {user?.imgUrl ? <img src={user.imgUrl} alt="" /> : <svg class="MuiSvgIcon-root MuiAvatar-fallback" focusable="false" viewBox="0 0 24 24" aria-hidden="true">
+          {user?.imgUrl ? <img src={user.imgUrl} alt="" /> : <svg className="MuiSvgIcon-root MuiAvatar-fallback" focusable="false" viewBox="0 0 24 24" aria-hidden="true">
             <path d="M12 12c2.21 0 4-1.79 4-4s-1.79-4-4-4-4 1.79-4 4 1.79 4 4 4zm0 2c-2.67 0-8 1.34-8 4v2h16v-2c0-2.66-5.33-4-8-4z">
             </path>
           </svg>}
