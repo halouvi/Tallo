89994c6d461a0eb47546cdc66405b899d999bda8
Arie Moldawsky <ariemol555@gmail.com>
2021-01-25 13:59:16 +0200

    automatic login

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,19 @@
+import { useEffect } from "react"
+import { useDispatch } from "react-redux"
 import { HomeFooter } from "../../components/Home/HomeFooter/HomeFooter"
 import { HomeGetStarted } from "../../components/Home/HomeGetStarted/HomeGetStarted"
 import { HomeHero } from "../../components/Home/HomeHero/HomeHero"
 import { HomeMain } from "../../components/Home/HomeMain/HomeMain"
 import { HomeQuotes } from "../../components/Home/HomeQuotes/HomeQuotes"
+import { LOGIN } from "../../store/user/UserActions"
 
 
 export const Home = (props) => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(LOGIN({ email: 'deni@avdija.com', password: '123' }));
+  }, [])
 
   return (
     <main className="home main-grid">
