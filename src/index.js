b2af59780b14fc3324fafb11f899eefb1f46db98
Arie Moldawsky <ariemol555@gmail.com>
2021-02-06 21:32:05 +0200

    regular DND

--- src/index.js
+++ src/index.js
@@ -8,43 +8,34 @@ import MouseBackEnd from 'react-dnd-mouse-backend'
 import { TouchBackend } from 'react-dnd-touch-backend'
 import { App } from './App.jsx'
 
-const hasNative = document && (document.elementsFromPoint || document.msElementsFromPoint)
+// const hasNative = document && (document.elementsFromPoint || document.msElementsFromPoint)
+// function getDropTargetElementsAtPoint(x, y, dropTargets) {
+//   return dropTargets.filter(t => {
+//     const rect = t.getBoundingClientRect()
+//     return (
+//       x >= rect.left &&
+//       x <= rect.right &&
+//       y <= rect.bottom &&
+//       y >= rect.top
+//     )
+//   })
+// }
 
-function getDropTargetElementsAtPoint(x, y, dropTargets) {
-  return dropTargets.filter(t => {
-    const rect = t.getBoundingClientRect()
-    return (
-      x >= rect.left &&
-      x <= rect.right &&
-      y <= rect.bottom &&
-      y >= rect.top
-    )
-  })
-}
-
-const backendOptions = {
-  getDropTargetElementsAtPoint: !hasNative && getDropTargetElementsAtPoint,
-  delayTouchStart: 3,
-  // scrollAngleRanges: [
-  //   { start: 30, end: 150 },
-  //   { start: 210, end: 330 },
-  //   { start: 300 }, { end: 60 }, { start: 120, end: 240 }
-  // ]
-}
-
-// const windowSize = () => {
-//   return window.innerWidth
-// } 
-
-// window.addEventListener('resize', () => {
-//   windowSize()
-//   console.log(windowSize());
-// })
+// const backendOptions = {
+//   getDropTargetElementsAtPoint: !hasNative && getDropTargetElementsAtPoint,
+//   delayTouchStart: 3,
+// scrollAngleRanges: [
+//   { start: 30, end: 150 },
+//   { start: 210, end: 330 },
+//   { start: 300 }, { end: 60 }, { start: 120, end: 240 }
+// ]
+// }
 
 render(
   <StrictMode>
     <ReduxProvider store={store}>
-      <DndProvider backend={window.innerWidth > 600 ? MouseBackEnd: TouchBackend}  options={backendOptions}>
+      {/* <DndProvider backend={window.innerWidth > 600 ? MouseBackEnd: TouchBackend}  options={backendOptions}> */}
+      <DndProvider backend={MouseBackEnd}>
         <RouterProvider>
           <App />
         </RouterProvider>
